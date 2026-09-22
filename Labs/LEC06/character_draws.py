ab2ca2297ed72db4c899f3eff123111006a4afa7
parkjihun216 <vlslrtm586@naver.com>
2026-09-23 00:42:10 +0900

    캐릭터 화면 출력

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,11 @@
+from pico2d import *
+
+
+open_canvas(800, 600)
+
+boy = load_image('character.png')
+
+
 def move_circle():
     print('circle')
 
@@ -10,8 +18,15 @@ def move_triangle():
     print('triangle')
 
 
+clear_canvas()
+boy.draw(400, 300)
+update_canvas()
+delay(1)
+
 while True:
     move_circle()
     move_rectangle()
     move_triangle()
     break
+
+close_canvas()
